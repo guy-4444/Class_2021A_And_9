9fd2b5f4c17535951c42f8cddf21486ab2158148
guymacin <guyskv@gmail.com>
2020-12-28 19:38:08 +0200

    Initial commit

---
+++ app/src/main/java/com/classy/class_2021a_and_9/Activity_Menu.java
@@ -0,0 +1,35 @@
+package com.classy.class_2021a_and_9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class Activity_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        Log.d("pttt", "Uid = " + firebaseUser.getUid());
+        Log.d("pttt", "DisplayName = " + firebaseUser.getDisplayName());
+        Log.d("pttt", "Email = " + firebaseUser.getEmail());
+        Log.d("pttt", "PhoneNumber = " + firebaseUser.getPhoneNumber());
+        Log.d("pttt", "PhotoUrl = " + firebaseUser.getPhotoUrl());
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName("Aliyahu Cohen")
+                .build();
+
+        firebaseUser.updateProfile(profileUpdates);
+        firebaseAuth.updateCurrentUser(firebaseUser);
+    }
+}
---
+++ app/src/main/java/com/classy/class_2021a_and_9/ListenerOptions.java
@@ -0,0 +1,41 @@
+package com.classy.class_2021a_and_9;
+
+public class ListenerOptions {
+}
+
+/*
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn.setOnClickListener(myClickListener);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    View.OnClickListener myClickListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            };
+}
+ */
